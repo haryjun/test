f3f2b8811116448aae7a26d102c2f54d3b89325b
haryjun <salah99@naver.com>
2024-11-20 17:16:43 +0900

    변경

---
+++ src/test/java/com/example/test1120/Test1120ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test1120;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1120ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
